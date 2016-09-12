ecbe4bdd1647dc99ed98ec7d724da206d254f929
Yunke Cao <ykcao@umich.edu>
2016-09-12 09:30:19 -0400

    finished

---
+++ Assets/__Scripts/Shield.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+	public float rotationsPersecond = 0.1f;
+	public bool _______;
+	public int levelShown = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int currLevel = Mathf.FloorToInt (Hero.S.shieldLevel);
+		if (levelShown != currLevel) {
+			levelShown = currLevel;
+			Material mat = this.GetComponent<Renderer>().material;
+			mat.mainTextureOffset = new Vector2 (0.2f * levelShown, 0);
+		}
+		float rZ = (rotationsPersecond * Time.time * 360) % 360f;
+		transform.rotation = Quaternion.Euler (0, 0, rZ);
+	}
+}
